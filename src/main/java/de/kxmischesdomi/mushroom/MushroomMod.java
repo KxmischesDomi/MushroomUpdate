073d56ae29f3c68e139117eb4f73e9bfd864fbd1
KxmischesDomi <kxmischesdomi@gmail.com>
2022-07-18 22:17:58 +0200

    Init modid

---
+++ src/main/java/de/kxmischesdomi/mushroom/MushroomMod.java
@@ -0,0 +1,17 @@
+package de.kxmischesdomi.mushroom;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MushroomMod implements ModInitializer {
+
+	public static final String MOD_ID = "mushroom";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
