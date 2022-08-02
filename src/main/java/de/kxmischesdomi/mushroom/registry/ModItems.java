c1c453efe887d4d1321959cae95235a674e0e386
KxmischesDomi <kxmischesdomi@gmail.com>
2022-08-02 21:06:54 +0200

    Fix 1.19 and 1.19.1 support

--- src/main/java/de/kxmischesdomi/mushroom/registry/ModItems.java
+++ src/main/java/de/kxmischesdomi/mushroom/registry/ModItems.java
@@ -2,17 +2,21 @@ package de.kxmischesdomi.mushroom.registry;
 
 import de.kxmischesdomi.mushroom.MushroomMod;
 import de.kxmischesdomi.mushroom.item.GlowMushroomStewItem;
-import de.kxmischesdomi.mushroom.item.MusicDiscItem;
 import de.kxmischesdomi.mushroom.item.ShroomGlider;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.minecraft.SharedConstants;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -32,7 +36,7 @@ public class ModItems {
 
 	public static final Item SHROOM_GLIDER = register("shroom_glider", new ShroomGlider(ArmorMaterials.LEATHER, EquipmentSlot.HEAD, new FabricItemSettings().group(TAB).maxCount(1).durability(100)));
 
-	public static final Item MUSIC_DISC_TWILIGHT = register("music_disc_twilight", new MusicDiscItem(1, ModSounds.MUSIC_DISC_TWILIGHT, new Item.Properties().stacksTo(1).tab(TAB).rarity(Rarity.RARE), 119));
+	public static final Item MUSIC_DISC_TWILIGHT = registerMusicDisc("music_disc_twilight", 1, ModSounds.MUSIC_DISC_TWILIGHT, 119, new FabricItemSettings().group(TAB).stacksTo(1).rarity(Rarity.RARE));
 
 	public static final Item SHROOM_PAL_SPAWN_EGG = register("shroom_pal_spawn_egg", new SpawnEggItem(ModEntities.SHROOM_PAL, 0x8B1A18, 0xFCBDBD, new FabricItemSettings().group(TAB)));
 	public static final Item PUFF_CREEPER_SPAWN_EGG = register("puff_creeper_spawn_egg", new SpawnEggItem(ModEntities.PUFF_CREEPER, 0xC5AB80, 0x5D5247, new FabricItemSettings().group(TAB)));
@@ -49,6 +53,33 @@ public class ModItems {
 
 	private static Item registerBlock(Block block) {
 		return register(Registry.BLOCK.getKey(block), new BlockItem(block, new Item.Properties().tab(TAB)));
+	}
+
+	/**
+	 * Creates a music disc item that has compatibility for 1.19 and 1.19.1 due to changes in the item's constructor
+	 */
+	private static Item registerMusicDisc(String name, int output, SoundEvent soundEvent, int length, Item.Properties properties) {
+
+		try {
+			RecordItem item;
+			String versionString = SharedConstants.getCurrentVersion().getName();
+			// Check if the version is newer or equal to 1.19.1
+			if (versionString.compareTo("1.19.1") >= 0) {
+				Constructor<RecordItem> constructor = RecordItem.class.getDeclaredConstructor(int.class, SoundEvent.class, Item.Properties.class, int.class);
+				constructor.setAccessible(true);
+				item = constructor.newInstance(output, soundEvent, properties, length);
+			} else {
+				Constructor<RecordItem> constructor = RecordItem.class.getDeclaredConstructor(int.class, SoundEvent.class, Item.Properties.class);
+				constructor.setAccessible(true);
+				item = constructor.newInstance(output, soundEvent, properties);
+			}
+			register(new ResourceLocation(MushroomMod.MOD_ID, name), item);
+		} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+			MushroomMod.LOGGER.error("Failed to register music disc item " + name + ": " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	public static void init() {
