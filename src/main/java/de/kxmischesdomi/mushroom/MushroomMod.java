59aae46cfe7108fd2ca6505998519f7f8918edaf
KxmischesDomi <kxmischesdomi@gmail.com>
2022-08-25 19:54:03 +0200

    Add music disc to fishing loottable

--- src/main/java/de/kxmischesdomi/mushroom/MushroomMod.java
+++ src/main/java/de/kxmischesdomi/mushroom/MushroomMod.java
@@ -4,8 +4,14 @@ import de.kxmischesdomi.mushroom.registry.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.advancements.critereon.LocationPredicate;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.predicates.LocationCheck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +35,16 @@ public class MushroomMod implements ModInitializer {
 
 		BiomeModifications.addSpawn(BiomeSelectors.includeByKey(Biomes.MUSHROOM_FIELDS), MobCategory.MONSTER, ModEntities.PUFF_CREEPER, 100, 1, 3);
 		BiomeModifications.addSpawn(BiomeSelectors.includeByKey(Biomes.MUSHROOM_FIELDS), MobCategory.CREATURE, ModEntities.SHROOM_PAL, 10, 4, 8);
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && BuiltInLootTables.FISHING_TREASURE.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.lootPool().add(
+						LootItem.lootTableItem(ModItems.MUSIC_DISC_TWILIGHT)
+						.when(LocationCheck.checkLocation(LocationPredicate.Builder.location().setBiome(ModBiomes.GLOW_CAVES)))
+				);
+				tableBuilder.withPool(poolBuilder);
+			}
+		});
 	}
 
 }
